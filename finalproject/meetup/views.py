9269f56febab7672288f2418c30749f1f839bfd1
Ameer Kiani <53153060+amirkiani@users.noreply.github.com>
2020-06-12 13:22:45 -0700

    Update views

--- finalproject/meetup/views.py
+++ finalproject/meetup/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from django.contrib.auth.decorators import login_required
+
+from .models import MeetUp, Activity
 
 # Starting with the indexrender
 def index(request):
@@ -8,4 +12,12 @@ def home(request):
     return render(request, 'base.html')
 
 def start(request):
-    return render(request, 'start,html')
+    return render(request, 'start.html')
+
+def getmeetup(request):
+    meetup_list=MeetUp.objects.all()
+    return render (request, 'meetup/meetup.html',{'meetup_list': meetup_list,})
+
+def getactivity(request):
+    activity_list=Activity.objects.all()
+    return render (request, 'meetup/activity.html',{'activity_list': activity_list,})
