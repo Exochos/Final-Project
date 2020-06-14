a860aca273f4d2fc7cf9259aaa402076cafe040f
Mimi2020hub <63885221+Mimi2020hub@users.noreply.github.com>
2020-06-13 21:58:21 -0700

    Update urls.py
     path('loginmessage/', views.loginmessage, name='loginmessage'),
     path('logoutmessage/', views.logoutmessage, name='logoutmessage'),

--- finalproject/meetup/urls.py
+++ finalproject/meetup/urls.py
@@ -4,5 +4,9 @@ from .views import *
 # Various Urls we will be using for the website
 urlpatterns=[
     path('', index, name='index'),
+    
+    
+    path('loginmessage/', views.loginmessage, name='loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
 
 ]
