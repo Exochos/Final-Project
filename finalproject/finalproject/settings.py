34238aa9133fe086efce1219e4abecaafa5ec31d
Mimi2020hub <63885221+Mimi2020hub@users.noreply.github.com>
2020-06-13 21:53:51 -0700

    Update settings.py
    LOGIN_REDIRECT_URL='loginmessage'
    LOGOUT_REDIRECT_URL='logoutmessage'

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -118,6 +118,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL='loginmessage'
+LOGOUT_REDIRECT_URL='logoutmessage'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
